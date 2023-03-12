92a640adf94146c6bbde7cae5606c3acafcc41bb
LewisG0400 <lewisgiannelli@hotmail.co.uk>
2023-03-12 16:15:54 +0000

    Upgraded map
    The position of the map is now set by a stream rather than manually
    polling and rebuiding with Futures.
    
    Added a marker to the user's position.

--- app/lib/map/map.dart
+++ app/lib/map/map.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -14,8 +16,7 @@ class Map extends StatefulWidget {
 
 class _MapHomeState extends State<Map> {
   late final MapController mapController;
-  Future<Position> currentLocation =
-      Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  Stream<Position> positionStream = Geolocator.getPositionStream(locationSettings: const LocationSettings(accuracy: LocationAccuracy.high));
 
   @override
   void initState() {
@@ -25,8 +26,8 @@ class _MapHomeState extends State<Map> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Position>(
-        future: currentLocation,
+    return StreamBuilder<Position>(
+        stream: positionStream,
         builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
           List<Widget> children;
           if (snapshot.hasData) {
@@ -50,6 +51,14 @@ class _MapHomeState extends State<Map> {
                         'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                     userAgentPackageName: 'com.example.app',
                   ),
+                  MarkerLayer(
+                    markers: [
+                      Marker(
+                        point: LatLng(snapshot.data!.latitude, snapshot.data!.longitude),
+                        builder: (ctx) => const Icon(Icons.my_location),
+                      )
+                    ],
+                  )
                 ],
               )
             ];
