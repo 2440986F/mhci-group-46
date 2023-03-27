c1ad3434f751da1db62c8e2355c2fbe1e759d3be
2385460g <2385460g@student.gla.ac.uk>
2023-03-27 19:13:22 +0100

    Added some options before starting the game thing

--- app/lib/game/game_instance.dart
+++ app/lib/game/game_instance.dart
@@ -40,43 +40,96 @@ class GameState extends State<GameInstance> {
   Widget build(BuildContext context) {
     switch (widget.state) {
       case _GameStates.SELECT_DISTANCE:
-        return Center(
-          child: FloatingActionButton(onPressed: () {
-            _setTarget(5).then((value) => setState(() {
-                  widget.state = _GameStates.PLAYING;
-                  widget.targetPosition = value;
-
-                  Isolate.spawn(_playSonarPing, [
-                    widget.receiveFromPingIsolate.sendPort,
-                    widget.targetPosition!,
-                    widget.audioPlayer,
-                  ]).then((isolate) => widget.pingIsolate = isolate);
-
-                  // widget.receiveFromPingIsolate
-                  //     .elementAt(0)
-                  //     .then((sendPort) => () {
-                  //           print("HGello");
-                  //           widget.sendToPingIsolate = sendPort;
-                  //           print(sendPort);
-                  //         });
-
-                  widget.receiveFromPingIsolate.listen((message) {
-                    if (message is SendPort) {
-                      widget.sendToPingIsolate = message;
-                    } else {
-                      widget.audioPlayer
-                          .play(AssetSource('sounds/sonar-ping-95840.mp3'));
-                      Geolocator.getLastKnownPosition().then((value) {
-                        widget.sendToPingIsolate
-                            ?.send([value!.latitude, value.longitude]);
-                      });
-                    }
-                  });
-                }));
-          }),
-        );
+        return Scaffold(
+            appBar: AppBar(title: const Text('Select Max Distance')),
+            body: Center(
+                child: ListView(padding: const EdgeInsets.all(8), children: [
+              Container(
+                  height: 50,
+                  color: Colors.blue,
+                  child: ElevatedButton(
+                      child: const Text('2.5km'),
+                      onPressed: () {
+                        _setTarget(2.5).then((value) => setState(() {
+                              widget.state = _GameStates.SELECT_DIFFICULTY;
+                              widget.targetPosition = value;
+                            }));
+                      })),
+              const Divider(),
+              Container(
+                  height: 50,
+                  color: Colors.blue,
+                  child: ElevatedButton(
+                      child: const Text('5km'),
+                      onPressed: () {
+                        _setTarget(5).then((value) => setState(() {
+                              widget.state = _GameStates.SELECT_DIFFICULTY;
+                              widget.targetPosition = value;
+                            }));
+                      })),
+              const Divider(),
+              Container(
+                  height: 50,
+                  color: Colors.blue,
+                  child: ElevatedButton(
+                      child: const Text('10km'),
+                      onPressed: () {
+                        _setTarget(10).then((value) => setState(() {
+                              widget.state = _GameStates.SELECT_DIFFICULTY;
+                              widget.targetPosition = value;
+                            }));
+                      })),
+            ])));
       case _GameStates.SELECT_DIFFICULTY:
-        break;
+        // This spawns a thread that keeps track of when to
+        // play the ping sound.
+        Isolate.spawn(_playSonarPing, [
+          widget.receiveFromPingIsolate.sendPort,
+          widget.targetPosition!,
+          widget.audioPlayer,
+        ]).then((isolate) => widget.pingIsolate = isolate);
+
+        // Can't play sound from within a thread so we need to
+        // get the new thread to send a message back to the main
+        // thread so that it can play the sound.
+        widget.receiveFromPingIsolate.listen((message) {
+          if (message is SendPort) {
+            widget.sendToPingIsolate = message;
+          } else {
+            widget.audioPlayer.play(AssetSource('sounds/sonar-ping-95840.mp3'));
+            Geolocator.getLastKnownPosition().then((value) {
+              widget.sendToPingIsolate
+                  ?.send([value!.latitude, value.longitude]);
+            });
+          }
+        });
+        return Scaffold(
+            appBar: AppBar(title: const Text('Select Difficulty')),
+            body: Center(
+                child: ListView(
+              padding: const EdgeInsets.all(8),
+              children: [
+                Container(
+                  height: 50,
+                  color: Colors.blue,
+                  child: ElevatedButton(
+                      child: const Text('Easy'),
+                      onPressed: () {
+                        setState(() => widget.state = _GameStates.PLAYING);
+                      }),
+                ),
+                const Divider(),
+                Container(
+                  height: 50,
+                  color: Colors.blue,
+                  child: ElevatedButton(
+                      child: const Text('Hard'),
+                      onPressed: () {
+                        setState(() => widget.state = _GameStates.PLAYING);
+                      }),
+                ),
+              ],
+            )));
       case _GameStates.PLAYING:
         // This is the timer that updates the sonar ping on the map.
         // Might find a better way of doing this, it is very laggy atm.
